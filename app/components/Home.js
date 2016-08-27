07508ad493537f2f4ded19b074e7c11d2474733f
Roland <roland.zeng@gmail.com>
2016-08-27 16:12:46 -0500

    moved important dependencies out of --save-dev

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,7 +4,7 @@ export default React.createClass({
     render () {
         return(
 
-            <p className='text-success'>this is our home page!!!!</p>
+            <p className='text-success'>this is our home page</p>
         )
     }
 
