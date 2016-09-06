4f7e8ec9a89b47cee6e874224add2a054c32aa79
Roland <roland.zeng@gmail.com>
2016-09-06 00:02:28 -0500

    Update About page

--- app/components/About.js
+++ app/components/About.js
@@ -8,15 +8,20 @@ function Changelog(props) {
             <h3><b>About</b></h3>
             <br />
 
-            <p>This is our about page.</p>
+            <p>Enter in your summoner name and region to view your current-game information.</p>
 
             <h3><b>Contact</b></h3>
             <br />
-            <p>Contact info here</p>
+            <p>Got questions / bugs / feedback? Please add us in-game:</p>
+            <p><b>Vanila</b></p>
+            <p><b>alexander wang</b></p>
+            <p></p>
 
             <h3><b>Legal</b></h3>
             <br />
-            <p>Legalities here</p>
+            <p>This site uses Google Analytics to track page views. It's the only cookie that we use!</p>
+            <br />
+            <p>LolCarry isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</p>
 
         </div>
     );
