cb5e10ead3b7bc50b3ee47120d7cd7ac039e0372
Roland <roland.zeng@gmail.com>
2016-09-28 14:54:57 -0700

    add sentry issue tracking for production mode ONLY

--- app/index.js
+++ app/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import routes from './config/routes';
+import Raven from 'raven-js';
+
+const ENV = ( process.env.NODE_ENV || 'development' ).trim();
+
+if (ENV === 'production') {
+    const sentryURL = process.env.SENTRY_URL || require('../SECRET').SENTRY_URL;
+
+    Raven.config(sentryURL).install();
+} else {
+    console.log('Raven issue tracking is DISABLED');
+}
+
 
 render(routes, document.getElementById('app'));
