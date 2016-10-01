a66a2329b26be23d89c7c4ea17c631be1f5e674e
Roland <roland.zeng@gmail.com>
2016-10-01 01:39:42 -0700

    add discord link to About page

--- app/components/About.js
+++ app/components/About.js
@@ -14,13 +14,18 @@ const Changelog = () =>
         <p>Got questions / bugs / feedback? Please add us in-game:</p>
         <p><b>Vanila</b></p>
         <p><b>alexander wang</b></p>
-        <p></p>
+        <br />
+        <p>Or, join the Discord chat room:</p>
+        <a href="https://discord.gg/mKTTFdd">
+        <img src="https://img.shields.io/badge/discord-lolcarry-blue.svg?style=flat" />
+        </a>
 
+        <br />
         <h3><b>Legal</b></h3>
         <br />
         <p>This site uses Google Analytics to track page views. It's the only cookie that we use!</p>
         <br />
-        <p>LolCarry isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</p>
+        <p><i>LolCarry isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</i></p>
 
     </div>
 
